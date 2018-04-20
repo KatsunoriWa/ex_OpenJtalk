2ff30bcea2a20ba06678ac7daefa9eecc769d68f
papa <gtx285aa@yahoo.co.jp>
2018-04-21 01:52:33 +0900

    use subprocess not shell script

--- ex_jtalk.py
+++ ex_jtalk.py
@@ -3,6 +3,8 @@
 # pylint: disable=C0103
 import os
 import time
+import subprocess
+
 import kana # https://github.com/haya14busa/twibusa/blob/master/romaji2kana.py
 
 def getAisatsu():
@@ -19,21 +21,47 @@ def getAisatsu():
 
     return aisatsu
 
-def jsay(message):
+
+
+def jtalk(t):
     u"""open_jtalk を使って日本語のメッセージを読み上げる
     message: 日本語の文字列
     """
+
+    # http://karaage.hatenadiary.jp/entry/2016/07/22/073000
+    open_jtalk=['open_jtalk']
+    mech=['-x','/var/lib/mecab/dic/open-jtalk/naist-jdic']
+    htsvoice=['-m','/usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice']
+#    htsvoice=['-m','mei_normal.htsvoice']
+    speed=['-r','1.0']
+    outwav=['-ow','open_jtalk.wav']
+    cmd=open_jtalk+mech+htsvoice+speed+outwav
+    c = subprocess.Popen(cmd,stdin=subprocess.PIPE)
+    c.stdin.write(t)
+    c.stdin.close()
+    c.wait()
+    aplay = ['aplay','-q','open_jtalk.wav']
+    wr = subprocess.Popen(aplay)
     
-    cmd = "sh jsay.sh %s" % message
-    os.system(cmd)
 
 if __name__ == "__main__":
     message = "たろうさん　こんにちは"
-    jsay(message)
+    jtalk(message)
+    time.sleep(1)
+
+    message = "お疲れ様。"
+    jtalk(message)
+    
+    time.sleep(1)
+
 
     names = ['naoto', 'makoto', 'osamu', 'tarou',
              'junya', 'masaaki', 'hideki',
              'akiko', 'sanae', 'kanako', 'shiori', 'yuka', 'ayaka']
+             
+    message = ""
     for name in names:
-        message = "%sさん　%s" % (kana.romaji2katakana(name), getAisatsu())
-        jsay(message)
+        message += "%sさん　%s　" % (kana.romaji2katakana(name), getAisatsu())
+
+    jtalk(message)
+        
